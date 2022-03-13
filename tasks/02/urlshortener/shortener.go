f8723dd5bdda962d11a7ff5cec0b048b1e868733
webostap <privet.ostap@mail.ru>
2022-03-13 18:22:40 +0300

    RYSSSKIE VPERED!

--- tasks/02/urlshortener/shortener.go
+++ tasks/02/urlshortener/shortener.go
@@ -1,25 +1,50 @@
 package urlshortener
 
 import (
+	"fmt"
+	"github.com/go-chi/chi"
+	"hash/fnv"
 	"net/http"
+	"strconv"
 )
 
+func hash(s string) uint64 {
+	h := fnv.New64()
+	h.Write([]byte(s))
+	return h.Sum64()
+}
+
 type URLShortener struct {
-	// TODO: More fields?
 	addr string
+	m    map[uint64]string
 }
 
 func NewShortener(addr string) *URLShortener {
 	return &URLShortener{
-		// TODO: Initialization for more fields?
 		addr: addr,
+		m:    make(map[uint64]string),
 	}
 }
 
 func (s *URLShortener) HandleSave(rw http.ResponseWriter, req *http.Request) {
-	// TODO: Implement me
+	url := req.URL.Query().Get("u")
+	h := hash(url)
+	_, found := s.m[h]
+	if !found {
+		s.m[h] = url
+		fmt.Fprintf(rw, "%s/%x", s.addr, h)
+	} else {
+		rw.WriteHeader(http.StatusInternalServerError)
+	}
 }
 
 func (s *URLShortener) HandleExpand(rw http.ResponseWriter, req *http.Request) {
-	// TODO: Implement me
+	k := chi.URLParam(req, "key")
+	h, _ := strconv.ParseUint(k, 16, 64)
+	v, found := s.m[h]
+	if found {
+		http.Redirect(rw, req, v, http.StatusMovedPermanently)
+	} else {
+		rw.WriteHeader(http.StatusNotFound)
+	}
 }
