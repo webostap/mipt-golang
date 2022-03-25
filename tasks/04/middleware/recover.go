704c11d62380dff6a3627a0acb5edd59752eb519
webostap <privet.ostap@mail.ru>
2022-03-26 01:04:44 +0300

    middleware1

--- tasks/04/middleware/recover.go
+++ tasks/04/middleware/recover.go
@@ -6,5 +6,16 @@ import (
 )
 
 func Recover(logger *log.Logger) func(http.Handler) http.Handler {
-	return nil
+	return func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			defer func() {
+				if r := recover(); r != nil {
+					logger.Println("[ERROR] Panic caught:", r)
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte("Internal Server Error\n"))
+				}
+			}()
+			handler.ServeHTTP(w, req)
+		})
+	}
 }
