50097a46287aee46c13a85bc12845d609ae664dc
ostap <privet.ostap@mail.ru>
2022-05-15 21:48:05 +0300

    fetchall2

--- tasks/05/httpfetch2/fetchall.go
+++ tasks/05/httpfetch2/fetchall.go
@@ -1,8 +1,10 @@
 package httpfetch2
 
 import (
+	"bytes"
 	"context"
 	"net/http"
+	"sync"
 )
 
 type Request struct {
@@ -17,7 +19,38 @@ type Result struct {
 }
 
 func FetchAll(ctx context.Context, c *http.Client, requests <-chan Request) <-chan Result {
-	сh := make(chan Result)
-	close(сh)
-	return сh
+	ch := make(chan Result)
+	var wg sync.WaitGroup
+	go func() {
+		defer func() {
+			wg.Wait()
+			close(ch)
+		}()
+		for {
+			select {
+			case request, ok := <-requests:
+				if ok {
+					wg.Add(1)
+					go func(req Request) {
+						defer wg.Done()
+						rr, _ := http.NewRequestWithContext(ctx, req.Method, req.URL, bytes.NewReader(req.Body))
+						var res Result
+						if resp, err := c.Do(rr); resp != nil {
+							defer resp.Body.Close()
+							res.StatusCode = resp.StatusCode
+						} else {
+							res.Error = err
+						}
+						ch <- res
+					}(request)
+				} else {
+					return
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	return ch
 }
