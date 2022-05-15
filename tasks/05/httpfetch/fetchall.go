551080997b6520bb5af68e717e5c832bc9ea251d
ostap <privet.ostap@mail.ru>
2022-05-15 18:31:31 +0300

    fetchall

--- tasks/05/httpfetch/fetchall.go
+++ tasks/05/httpfetch/fetchall.go
@@ -2,6 +2,7 @@ package httpfetch
 
 import (
 	"net/http"
+	"sync"
 )
 
 type Request struct {
@@ -16,5 +17,19 @@ type Result struct {
 }
 
 func FetchAll(c *http.Client, requests []Request) []Result {
-	return nil
+	results := make([]Result, len(requests))
+	var wg sync.WaitGroup
+	for idx := range requests {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			if resp, err := c.Get(requests[i].URL); err != nil {
+				results[i].Error = err
+			} else {
+				results[i].StatusCode = resp.StatusCode
+			}
+		}(idx)
+	}
+	wg.Wait()
+	return results
 }
